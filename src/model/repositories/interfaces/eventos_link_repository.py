2abad088028f40b31488247f89b40d964ab8aa49
MateusJSouza <mateusjdev@gmail.com>
2025-02-21 20:00:48 -0300

    feat: implementing eventos_link_repository

---
+++ src/model/repositories/interfaces/eventos_link_repository.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+from src.model.entities.eventos_link import EventosLink
+
+class EventosRepositoryInterface(ABC):
+  @abstractmethod
+  def insert(self, event_id: str, subscriber_id: int) -> str: pass
+  
+  @abstractmethod
+  def select_events_link(self, event_id: str, subscriber_id: int) -> EventosLink: pass
