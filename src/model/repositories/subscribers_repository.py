58b259ac2b69b075b868751ab5043f9761c38008
MateusJSouza <mateusjdev@gmail.com>
2025-02-21 13:20:41 -0300

    feat: implementing controllers

--- src/model/repositories/subscribers_repository.py
+++ src/model/repositories/subscribers_repository.py
@@ -7,26 +7,27 @@ class SubscribersRepository(SubscribersRepositoryInterface):
     with DBConnectionHandler() as db:
       try:
         new_subscriber = Inscritos(
-          nome=subscriber_infos.get("name"),
-          email=subscriber_infos.get("email"),
-          link=subscriber_infos.get("link"),
-          evento_id=subscriber_infos.get("evento_id"),
-        )
+                nome=subscriber_infos.get("name"),
+                email=subscriber_infos.get("email"),
+                link=subscriber_infos.get("link"),
+                evento_id=subscriber_infos.get("evento_id")
+            )
         db.session.add(new_subscriber)
         db.session.commit()
       except Exception as exception:
         db.session.rollback()
         raise exception
-  
+
   def select_subscriber(self, email: str, evento_id: int) -> Inscritos:
     with DBConnectionHandler() as db:
       data = (
         db.session
-          .query(Inscritos)
-          .filter(
+        .query(Inscritos)
+        .filter(
             Inscritos.email == email,
             Inscritos.evento_id == evento_id
-          )
-          .one_or_none()
+        )
+        .one_or_none()
       )
+
       return data
--- src/model/repositories/subscribers_repository_test.py
+++ src/model/repositories/subscribers_repository_test.py
@@ -1,23 +1,20 @@
-import pytest
-
 from .subscribers_repository import SubscribersRepository
 
-@pytest.mark.skip("Insert in DB")
-def test_insert_eventos():
+def test_insert():
   subscriber_info = {
-    "name": "meuNome222",
-    "email": "teste22@gmail.com",
-    "evento_id": 2
+      "name": "meuNome222",
+      "email": "email22@email.com",
+      "evento_id": 2
   }
-  
+
   subs_repo = SubscribersRepository()
   subs_repo.insert(subscriber_info)
-  
+
+
 def test_select_subscriber():
-  email = "teste@gmail.com"
+  email = "email@email.com"
   evento_id = 2
-  
+
   subs_repo = SubscribersRepository()
   resp = subs_repo.select_subscriber(email, evento_id)
-  
   print(resp.nome)
